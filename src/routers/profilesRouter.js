9b0dda333920a22f7f08734f9209df80b3cc26ca
Thomas Mazzotta <thomasmazzotta99@gmail.com>
2023-12-20 13:15:10 +0100

    Updates! (click me)
    - aggiunto post di login
    - sistemata modifica utente, adesso se c'è una nuova pssw la hasha nuovamente
    - sistemati problemi con prettier, mi stava facendo impazzire

--- src/routers/profilesRouter.js
+++ src/routers/profilesRouter.js
@@ -41,13 +41,43 @@ profilesRouter
       next(err)
     }
   })
-  //questo GET ritorna il profile dell'utente LOGGATO. Qui viene controllato il token
-  //in assenza di token si confrontano le password con bcript e si genera un altro token
-  //Requisiti: token, email, password
+  //questo GET ritorna le informazioni dell'utente se ha un token valido
+  //Requisiti: token
   //checkJWT fornisce già l'utente
   .get("/me", checkJwt, async (req, res, next) => {
     try {
       res.status(200).json(req.user)
+    } catch (err) {
+      next(err)
+    }
+  })
+
+  //questo psot serve ad effettuare il login con password ed email,
+  //Requisiti: password ed email (funziona anche se c'è tutto il resto dell'oggetto)
+  //Se tutto funziona correttamente da un token valido per 5 ore
+  .post("/login", async (req, res, next) => {
+    try {
+      const { email, password } = req.body
+
+      // trova l'utente tramite email
+      const user = await User.findOne({ email })
+      if (!user) {
+        return res.status(404).json({ message: "User not found" })
+      }
+
+      // confronta la password fornita con quella hashata nel db
+      const isPasswordCorrect = await bcrypt.compare(password, user.password)
+      if (!isPasswordCorrect) {
+        return res.status(401).json({ message: "Invalid credentials" })
+      }
+
+      // Genera un nuovo token JWT
+      const token = jwt.sign({ userId: user._id }, process.env.MY_SECRET, {
+        expiresIn: "5h", // Imposta un periodo di validità per il token
+      })
+
+      // Restituisce l'utente con il nuovo token
+      res.status(200).json({ token })
     } catch (err) {
       next(err)
     }
@@ -70,25 +100,18 @@ profilesRouter
     }
   })
   //questa patch carica l'immagine sul server in uploads e resistuisce la posizione dell'immagine
-  .patch(
-    "/:id/image",
-    checkJwt,
-    upload.single("profile-img"),
-    async (req, res, next) => {
-      try {
-        if (req.file) {
-          console.log(req.file.path) // Stampa il percorso dove viene salvato il file
-          res
-            .status(200)
-            .json({ message: "Immagine caricata!", path: req.file.path })
-        } else {
-          res.status(400).json({ message: "No file uploaded" })
-        }
-      } catch (error) {
-        next(error) // Passa eventuali errori al middleware di gestione degli errori
+  .patch("/:id/image", checkJwt, upload.single("profile-img"), async (req, res, next) => {
+    try {
+      if (req.file) {
+        console.log(req.file.path) // Stampa il percorso dove viene salvato il file
+        res.status(200).json({ message: "Immagine caricata!", path: req.file.path })
+      } else {
+        res.status(400).json({ message: "No file uploaded" })
       }
+    } catch (error) {
+      next(error) // Passa eventuali errori al middleware di gestione degli errori
     }
-  )
+  })
   //questo POST crea un nuovo utente, hasha la password, e resituisce subito il token (REGISTRAZIONE)
   //Requisiti body: name surname email password
   .post("/", async (req, res, next) => {
@@ -113,9 +136,16 @@ profilesRouter
   //questa PUT modifica l'utente se esso è autorizzato, tuttavia è necessario hashare nuovamente la pssw
   .put("/:id", checkJwt, async (req, res, next) => {
     try {
-      const updatedUser = await User.findByIdAndUpdate(req.user._id, req.body, {
+      //creo una variabile in modo tale da copiare la richiesta inserendo però la pssw hashata
+      let updatedBodyHashed = req.body
+      if (req.body.password) {
+        updatedBodyHashed.password = await bcrypt.hash(req.body.password, 10)
+      }
+
+      const updatedUser = await User.findByIdAndUpdate(req.user._id, updatedBodyHashed, {
         new: true,
-      })
+        runValidators: true,
+      }).select("-__v -password")
 
       res.json(updatedUser)
     } catch (err) {
